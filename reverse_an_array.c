b0acb09680bebb37b3f15602231be18f3785b9dd
Rudra Verma <rudra9379@gmail.com>
2026-01-04 20:30:07 +0530

    Update reverse_an_array.c

--- reverse_an_array.c
+++ reverse_an_array.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() {
     int n, i, arr[100], temp;
-    printf("Enter the number of elements");3
+    printf("Enter the number of elements");
      scanf("%d", &n);
 
      for(i = 0; i < n; i++)
